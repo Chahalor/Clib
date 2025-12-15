c052c6e73a3ccc1de8246c145c32d88558a2789c
nduvoid <nathan@duvoid.fr>
2025-12-15 19:39:22 +0100

    feat(random): add the random module

---
+++ random/random.c
@@ -0,0 +1,75 @@
+// Header
+
+/* ----| Headers    |----- */
+	/* Standard */
+#include <stdlib.h>
+#include <time.h>
+
+	/* Internal */
+#include "random.h"
+
+	/* External */
+#include "../math/math.h"
+#include "../memory/memory.h"
+
+/* ----| Prototypes |----- */
+	//...
+
+/* ----| Internals  |----- */
+
+/**
+ * @brief	init the srand() has a constructor
+ * 
+ * @version	1.0
+ */
+__attribute__((constructor))
+static void	random_init_seed(void)
+{
+	srand(time(NULL));
+}
+
+/* ----| Public     |----- */
+
+/**
+ * @brief	return a value between val1 and val2
+ * 
+ * @param	val1	first value
+ * @param	val2	second value
+ * 
+ * @return a random value
+ * 
+ * @version 0.0.1
+ */
+// __attribute__(())
+int	randint(
+	const int _val1,
+	const int _val2
+)
+{
+	const int	_val = rand();
+	const int	_min = min(_val1, _val2);
+	const int	_max = max(_val1, _val2);
+
+	return (_val % ((_max - _min) + _min));
+}
+
+/**
+ * 
+*/
+__attribute__((malloc))
+int	*rand_range(
+	const unsigned int _size,
+	const int _val1,
+	const int _val2
+)
+{
+	int				*result = NULL;
+	unsigned int	_i = 0;
+
+	result = mem_alloc(_size * sizeof(int));
+	if (unlikely(!result))
+		return (NULL);
+	while (_i < _size)
+		result[_i] = randint(_val1, _val2);
+	return (result);
+}
