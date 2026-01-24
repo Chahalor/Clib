c19fd7edbb9288c6f5f81fcce045b8e89e767ccd
nduvoid <nathan@duvoid.fr>
2026-01-24 12:09:09 +0100

    feat(interface/args): _args_mem_new_option()

--- interface/args/_internal_/_args_memory.c
+++ interface/args/_internal_/_args_memory.c
@@ -87,3 +87,47 @@ _t_args_parser	*_args_mem_new_parser(
 error:
 	return (new);
 }
+
+_t_args_option	*_args_mem_new_option(
+	const char *const _long_name,
+	const char _short_name,
+	const char *const _desc
+)
+{
+	const size_t	_name_length = _long_name ? strlen(_long_name) : 0;
+	const size_t	_desc_length = _desc ? strlen(_desc) : 0;
+	const size_t	_alloc_size = sizeof(_t_args_parser) + sizeof(char) * (_name_length);
+	_t_args_option	*new;
+
+	new = mem_alloc(_alloc_size);
+	if (unlikely(!new))
+	{
+		_args_config_set_errnum(error_alloc_fail);
+		goto error;
+	}
+
+	if (_long_name)
+	{
+		new->long_name = (char *)(new + 1);
+		strcpy(new->long_name, _long_name);
+	}
+	if (_desc)
+	{
+		new->desc = mem_alloc(_desc_length);
+		if (unlikely(!new->desc))
+		{
+			_args_config_set_errnum(error_alloc_fail);
+			mem_free(new);
+			goto error;
+		}
+		else
+			strcpy(new->desc, _desc);
+	}
+
+	new->next = NULL;
+	new->params = NULL;
+	new->short_name = _short_name;
+
+error:
+	return (new);
+}
